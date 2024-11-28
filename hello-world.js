784dd8ad6d36a265a126324c1fe3f350aeea3248
juliapolbach <julia@alternaenergetica.com>
2024-11-28 12:55:25 +0100

    Added Hello World challenge

---
+++ hello-world.js
@@ -0,0 +1,9 @@
+/*  HELLO WORLD (Exercise 1 of 13)  
+
+  Write a program that prints the text "HELLO WORLD" to the console (stdout).
+
+Written by: Júlia Polbach
+https://github.com/jujuvideogirlai
+*/
+
+console.log("HELLO WORLD");
