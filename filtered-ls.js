405beb6cab4cc1e8124225165488d9c70385d26b
juliapolbach <julia@alternaenergetica.com>
2024-12-17 12:07:43 +0100

    Filtered-ls challenge

---
+++ filtered-ls.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const dirPath = process.argv[2]
+const ext = process.argv[3]
+
+fs.readdir(dirPath, (err, list) => {
+  if (err) return console.error(err)
+
+  list.filter(file => path.extname(file) === `.${ext}`)
+    .forEach(file => console.log(file))
+})
